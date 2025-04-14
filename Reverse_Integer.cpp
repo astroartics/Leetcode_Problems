8649c40019355b1563132fa0cb27cc97f9c2f67f
Sanjana Joshi <134907864+astroartics@users.noreply.github.com>
2025-04-14 12:59:13 +0530

    Update Reverse_Integer.cpp

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long int ans = 0;
+        
+        while(x != 0)
+        {
+            long int last = x % 10;
+            ans = ans * 10 + last;
+            x /= 10;
+        }
 
+        if((ans > (pow(2,31))) || (ans < pow(-2,31)))
+            return 0;
+        return ans;
+    }
+};
