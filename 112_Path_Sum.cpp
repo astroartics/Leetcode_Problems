3bf284d683c2514fa484e8eae94a6577bd7afe76
Sanjana Joshi <134907864+astroartics@users.noreply.github.com>
2025-05-05 15:59:43 +0530

    Create 112_Path_Sum.cpp

---
+++ 112_Path_Sum.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(!root) return false;
+
+        if(root->val == targetSum && !root->left && !root->right)
+        {
+            return true;
+        }
+
+        bool l = hasPathSum(root->left, targetSum - root->val);
+        bool r = hasPathSum(root->right, targetSum - root->val);
+
+        return l || r;
+        // That is at least one path was true, that is it had the leaf value as the final tergetSum - root->val
+    }
+};
